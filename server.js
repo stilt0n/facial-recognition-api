fa1d919be80e1fd05272a9d32928f458b496ae0a
stilt0n <mattias.leino@arts.cornish.edu>
2018-11-13 14:44:49 -0700

    updated database info

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ const knex = require('knex')
 const db = knex({
 	client: 'pg',
 	connection: {
-		host : '127.0.0.1',
+		host : 'postgresql-clean-40557',
 		user: 'postgres',
 		password: '',
 		database: 'image-recognition'
